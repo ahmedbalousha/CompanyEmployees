b1bdf10f5c1b5d74f74b012a91b6d9ab6419c509
ahmedbalousha <eng.ahmedbalousha@outlook.com>
2022-10-14 01:59:20 +0300

    add

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        ////private ILoggerManager _logger;
+        ////public WeatherForecastController(ILoggerManager logger)
+        ////{
+        ////    _logger = logger;
+        ////}
+
+        ////[HttpGet]
+        ////public IEnumerable<string> Get()
+        ////{
+        ////    _logger.LogInfo("Here is info message from our values controller.");
+        ////    _logger.LogDebug("Here is debug message from our values controller.");
+        ////    _logger.LogWarn("Here is warn message from our values controller.");
+        ////    _logger.LogError("Here is an error message from our values controller.");
+        ////    return new string[] { "value1", "value2" };
+        ////}
+        private readonly IRepositoryManager _repository;
+        public WeatherForecastController(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _repository.Company.ToString();
+            _repository.Employee.ToString();
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+
+}
---
+++ CompanyEmployees/Startup.cs
@@ -0,0 +1,156 @@
+using CompanyEmployees.ActionFilters;
+using CompanyEmployees.Extensions;
+using CompanyEmployees.Utility;
+using Contracts;
+using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using NLog;
+using Repository.DataShaping;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+            "/nlog.config"));
+
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+
+        {
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+            services.ConfigureLoggerService();
+            services.ConfigureSqlContext(Configuration);
+            services.ConfigureRepositoryManager();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Orders", Version = "v1" });
+                c.AddSecurityDefinition("Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Description = "Please enter into field the word 'Bearer' following by space and JWT",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Scheme = "Bearer"
+                    });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+            services.AddAutoMapper(typeof(Startup));
+            services.AddMvc(options =>
+            {
+                options.SuppressAsyncSuffixInActionNames = false;
+            });
+
+            services.AddControllers(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+
+            }).AddNewtonsoftJson()
+                .AddXmlDataContractSerializerFormatters()
+              .AddCustomCSVFormatter();
+            services.AddCustomMediaTypes();
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+            services.AddScoped<ValidationFilterAttribute>();
+            services.AddScoped<ValidateCompanyExistsAttribute>();
+            services.AddScoped<IDataShaper<EmployeeDto>, DataShaper<EmployeeDto>>();
+            services.AddScoped<EmployeeLinks>();
+            services.AddScoped<ValidateMediaTypeAttribute>();
+
+
+
+
+        }
+
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ILoggerManager logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+              //  app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            // app.ConfigureExceptionHandler(logger);
+          //  app.ConfigureExceptionHandler(logger);
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
+            });
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CompanyEmployees");
+            });
+
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
+        }
+    }
+}
